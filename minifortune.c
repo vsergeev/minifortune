cd354ee95ba87d34bee7b2a0a20f7146d2ed160e
Vanya A. Sergeev <v@sergeev.io>
2016-10-03 22:54:42 -0700

    clean up code and error messages

--- minifortune.c
+++ minifortune.c
@@ -45,6 +45,7 @@ struct fortune_dat_header {
 #define DAT_STR_FLAG_ORDERED   0x2      /* ordered pointers */
 #define DAT_STR_FLAG_ROTATED   0x4      /* rot-13'd text */
 
+#ifdef DEBUG
 void dat_header_dump(const struct fortune_dat_header *dat_header) {
     printf("str_version: %d\n", dat_header->str_version);
     printf("str_numstr: %d\n", dat_header->str_numstr);
@@ -53,6 +54,7 @@ void dat_header_dump(const struct fortune_dat_header *dat_header) {
     printf("str_flags: %d\n", dat_header->str_flags);
     printf("str_delim: %c\n", dat_header->str_delim);
 }
+#endif
 
 /******************************************************************************/
 /* Utility functions to choose a random path, dat file, and fortune position. */
@@ -104,34 +106,34 @@ int choose_random_path(char *path, size_t maxlen, const char *paths) {
 
 /* Boolean filter for .dat extension for use with scandir() */
 int filter_extension_dat(const struct dirent *d) {
-    /* File name length must be at least: "*.dat" */
-    /* File name must end with ".dat" */
+    /* File name must be at least: "*.dat" */
     size_t len = strlen(d->d_name);
-    return (len >= 5) && (strncmp(d->d_name + (len - 4), ".dat", 4) == 0);
+    return (len > 4) && (strncmp(d->d_name + (len - 4), ".dat", 4) == 0);
 }
 
 /* Choose random .dat file from directory dir_path. */
 int choose_random_datfile(char *dat_path, int maxlen, const char *dir_path) {
-    struct dirent **fnamelist;
     int n;
+    struct dirent **fnamelist;
 
     /* Form a list of .dat files in directory dir_path */
     n = scandir(dir_path, &fnamelist, filter_extension_dat, alphasort);
     if (n < 0) {
-        fprintf(stderr, "Error reading fortune directory '%s': scandir(): %s\n", dir_path, strerror(errno));
+        fprintf(stderr, "Error reading fortune directory \"%s\": scandir(): %s\n", dir_path, strerror(errno));
         return -1;
     }
 
     /* No .dat files found */
-    if (n == 0)
+    if (n == 0) {
+        free(fnamelist);
         return -1;
-
-    if (n > 0) {
-        /* Pick a random .dat file */
-        unsigned int i = rand() % n;
-        /* Assemble the .dat file path */
-        snprintf(dat_path, maxlen, "%s/%s", dir_path, fnamelist[i]->d_name);
     }
+
+    /* Pick a random .dat file from [0, n-1] */
+    unsigned int index = rand() % n;
+
+    /* Assemble the .dat file path */
+    snprintf(dat_path, maxlen, "%s/%s", dir_path, fnamelist[index]->d_name);
 
     /* Free our filename list structure */
     for (int i = 0; i < n; i++)
@@ -144,18 +146,17 @@ int choose_random_datfile(char *dat_path, int maxlen, const char *dir_path) {
 /* Choose a random fortune position from a .dat file dat_path. */
 int choose_random_fortune_pos(uint32_t *pos, uint8_t *delim, bool *rot13, const char *dat_path) {
     FILE *fp;
-    struct fortune_dat_header dat_header;
-    uint32_t fortune_id, fortune_pos;
 
     /* Open the .dat file */
     if ((fp = fopen(dat_path, "r")) == NULL) {
-        fprintf(stderr, "Error opening fortune dat file '%s': fopen(): %s\n", dat_path, strerror(errno));
+        fprintf(stderr, "Error opening fortune .dat file \"%s\": fopen(): %s\n", dat_path, strerror(errno));
         goto cleanup_failure;
     }
 
     /* Read the .dat file header */
+    struct fortune_dat_header dat_header;
     if (fread(&dat_header, sizeof(struct fortune_dat_header), 1, fp) < 1) {
-        fprintf(stderr, "Error reading fortune dat header from '%s': fread(): %s\n", dat_path, strerror(errno));
+        fprintf(stderr, "Error reading fortune .dat header from \"%s\": fread(): %s\n", dat_path, strerror(errno));
         goto cleanup_failure;
     }
 
@@ -172,29 +173,31 @@ int choose_random_fortune_pos(uint32_t *pos, uint8_t *delim, bool *rot13, const 
 
     /* Check the .dat file header */
     if (dat_header.str_version != 1 && dat_header.str_version != 2) {
-        fprintf(stderr, "Error, unsupported .dat header version %d in '%s'.\n", dat_header.str_version, dat_path);
+        fprintf(stderr, "Error, unsupported .dat header version %d in \"%s\".\n", dat_header.str_version, dat_path);
         goto cleanup_failure;
     }
 
-    /* Pick a random fortune */
-    fortune_id = rand() % dat_header.str_numstr;
+    uint32_t fortune_pos;
+
+    /* Pick a random fortune from [0, str_numstr-1] */
+    unsigned int index = rand() % dat_header.str_numstr;
 
     /* Seek to the fortune position in the .dat file positions table */
-    if (fseek(fp, fortune_id*4, SEEK_CUR) < 0) {
-        fprintf(stderr, "Error seeking to fortune id %d in '%s': fseek(): %s\n", fortune_id, dat_path, strerror(errno));
+    if (fseek(fp, index*sizeof(fortune_pos), SEEK_CUR) < 0) {
+        fprintf(stderr, "Error seeking to fortune index %d in \"%s\": fseek(): %s\n", index, dat_path, strerror(errno));
         goto cleanup_failure;
     }
 
     /* Read the fortune position */
-    if (fread(&fortune_pos, 4, 1, fp) < 1) {
-        fprintf(stderr, "Error reading fortune id %d in '%s': fread: %s\n", fortune_id, dat_path, strerror(errno));
+    if (fread(&fortune_pos, sizeof(fortune_pos), 1, fp) < 1) {
+        fprintf(stderr, "Error reading fortune index %d in \"%s\": fread(): %s\n", index, dat_path, strerror(errno));
         goto cleanup_failure;
     }
     fortune_pos = ntohl(fortune_pos);
 
     *pos = fortune_pos;
     *delim = dat_header.str_delim;
-    *rot13 = ((dat_header.str_flags & DAT_STR_FLAG_ROTATED) != 0);
+    *rot13 = (dat_header.str_flags & DAT_STR_FLAG_ROTATED) != 0;
 
     fclose(fp);
     return 0;
@@ -205,46 +208,55 @@ int choose_random_fortune_pos(uint32_t *pos, uint8_t *delim, bool *rot13, const 
     return -1;
 }
 
+bool isdir(const char *path) {
+    struct stat st_buf;
+
+    if (stat(path, &st_buf) < 0)
+        return false;
+
+    return S_ISDIR(st_buf.st_mode) != 0;
+}
+
 /******************************************************************************/
 /* Read fortune function. */
 /******************************************************************************/
 
-/* Read a fortune from fortune file fortune_path at position pos and delimiter delim. */
+/* Read a fortune from fortune file fortune_path at position pos with delimiter delim. */
 int read_fortune(char **fortune, const char *fortune_path, uint32_t pos, uint8_t delim, bool rot13) {
     FILE *fp;
-    int len, c;
-    char search[2] = {0};
 
     *fortune = NULL;
 
     /* Open the fortune file */
     if ((fp = fopen(fortune_path, "r")) == NULL) {
-        fprintf(stderr, "Error opening fortune file '%s': fopen(): %s\n", fortune_path, strerror(errno));
+        fprintf(stderr, "Error opening fortune file \"%s\": fopen(): %s\n", fortune_path, strerror(errno));
         goto cleanup_failure;
     }
 
     /* Seek to the fortune position */
     if (fseek(fp, pos, SEEK_SET) < 0) {
-        fprintf(stderr, "Error seeking in fortune file '%s': fseek(): %s\n", fortune_path, strerror(errno));
+        fprintf(stderr, "Error seeking fortune file \"%s\": fseek(): %s\n", fortune_path, strerror(errno));
         goto cleanup_failure;
     }
 
     /* Count the fortune length up to the delimiter */
+    unsigned int len;
+    int c = 0, prev_c = 0;
     for (len = 0; ; len++) {
         if ((c = fgetc(fp)) == EOF) {
             if (feof(fp))
-                fprintf(stderr, "Error reading fortune from '%s': Delimiter not found.\n", fortune_path);
+                fprintf(stderr, "Error reading fortune from \"%s\": End delimiter not found.\n", fortune_path);
             else if (ferror(fp))
-                fprintf(stderr, "Error reading fortune from '%s': fgetc(): %s\n", fortune_path, strerror(errno));
+                fprintf(stderr, "Error reading fortune from \"%s\": fgetc(): %s\n", fortune_path, strerror(errno));
             goto cleanup_failure;
         }
-        search[0] = search[1];
-        search[1] = (char)c;
-        if (search[0] == delim && search[1] == '\n') {
-            len--;
+
+        if (prev_c == delim && c == '\n')
             break;
-        }
+
+        prev_c = c;
     }
+    len -= 1;
 
     /* Allocate memory for the fortune */
     if ((*fortune = malloc(len+1)) == NULL) {
@@ -254,13 +266,13 @@ int read_fortune(char **fortune, const char *fortune_path, uint32_t pos, uint8_t
 
     /* Seek back to the fortune position */
     if (fseek(fp, pos, SEEK_SET) < 0) {
-        fprintf(stderr, "Error seeking in fortune file '%s': fseek(): %s\n", fortune_path, strerror(errno));
+        fprintf(stderr, "Error seeking in fortune file \"%s\": fseek(): %s\n", fortune_path, strerror(errno));
         goto cleanup_failure;
     }
 
     /* Read the fortune */
     if (fread(*fortune, 1, len, fp) < len) {
-        fprintf(stderr, "Error reading fortune from '%s': fread(): %s\n", fortune_path, strerror(errno));
+        fprintf(stderr, "Error reading fortune from \"%s\": fread(): %s\n", fortune_path, strerror(errno));
         goto cleanup_failure;
     }
 
@@ -288,33 +300,16 @@ int read_fortune(char **fortune, const char *fortune_path, uint32_t pos, uint8_t
     return -1;
 }
 
-bool isdir(const char *path) {
-    struct stat st_buf;
-
-    if (stat(path, &st_buf) < 0)
-        return false;
-
-    if (S_ISDIR(st_buf.st_mode))
-        return true;
-
-    return false;
-}
-
 int main(int argc, char *argv[]) {
     char dat_path[PATH_MAX];
 
-    uint32_t fortune_pos;
-    uint8_t fortune_delim;
-    bool fortune_rot13;
-    char *fortune = NULL;
-
     /* Help/Usage */
     if (argc == 2 && ((strcmp("-h", argv[1]) == 0) || (strcmp("--help", argv[1]) == 0))) {
-        printf("Usage: %s [path to fortune file or directory]\n"
+        printf("Usage: %s [path to fortune file or directory]\n\n"
                "Version 2.4 - https://github.com/vsergeev/minifortune\n\n"
                "If no fortune file or directory is specified, minifortune defaults to:\n\n"
-               "    %s         environment variable containing one or\n"
-               "                         more colon-separated files or directories\n\n"
+               "    %s         environment variable containing one or more\n"
+               "                         colon-separated files or directories\n\n"
                "    %s   directory\n\n", argv[0], ENV_FORTUNE_PATH, DEF_FORTUNE_DIR);
         exit(EXIT_SUCCESS);
     }
@@ -333,14 +328,14 @@ int main(int argc, char *argv[]) {
 
             /* Look up a random path from the colon separated paths in the environment variable */
             if (choose_random_path(path, sizeof(path), getenv(ENV_FORTUNE_PATH)) < 0) {
-                fprintf(stderr, "Error, no path found in paths '%s'.\n", getenv(ENV_FORTUNE_PATH));
+                fprintf(stderr, "Error, no path found in environment variable paths: %s\n", getenv(ENV_FORTUNE_PATH));
                 exit(EXIT_FAILURE);
             }
 
             if (isdir(path)) {
                 /* Look up a random .dat file in the directory */
                 if (choose_random_datfile(dat_path, sizeof(dat_path), path) < 0) {
-                    fprintf(stderr, "Error, no fortune file found in directory '%s'.\n", path);
+                    fprintf(stderr, "Error, no fortune file found in directory: \"%s\"\n", path);
                     exit(EXIT_FAILURE);
                 }
             } else {
@@ -352,7 +347,7 @@ int main(int argc, char *argv[]) {
 
             /* Look up a random .dat file in the directory */
             if (choose_random_datfile(dat_path, sizeof(dat_path), DEF_FORTUNE_DIR) < 0) {
-                fprintf(stderr, "Error, no fortune file found in directory '%s'.\n", DEF_FORTUNE_DIR);
+                fprintf(stderr, "Error, no fortune file found in directory: \"%s\"\n", DEF_FORTUNE_DIR);
                 exit(EXIT_FAILURE);
             }
         } else {
@@ -368,7 +363,7 @@ int main(int argc, char *argv[]) {
 
             /* Look up a random .dat file in the directory */
             if (choose_random_datfile(dat_path, sizeof(dat_path), argv[1]) < 0) {
-                fprintf(stderr, "Error, no fortune file found in directory '%s'.\n", argv[1]);
+                fprintf(stderr, "Error, no fortune .dat file found in directory \"%s\".\n", argv[1]);
                 exit(EXIT_FAILURE);
             }
         } else {
@@ -378,6 +373,11 @@ int main(int argc, char *argv[]) {
             snprintf(dat_path, sizeof(dat_path), "%s.dat", argv[1]);
         }
     }
+
+    uint32_t fortune_pos;
+    uint8_t fortune_delim;
+    bool fortune_rot13;
+    char *fortune = NULL;
 
     /* Choose a random fortune position from the .dat file */
     if (choose_random_fortune_pos(&fortune_pos, &fortune_delim, &fortune_rot13, dat_path) < 0)
@@ -397,4 +397,3 @@ int main(int argc, char *argv[]) {
 
     exit(EXIT_SUCCESS);
 }
-
